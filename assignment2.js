4ed9bed710f32593b302dcf3c4afad7008728d66
pinklz <116235230+pinklz@users.noreply.github.com>
2023-11-11 18:03:25 -0800

    Playing with setting background, add walkway

--- assignment2.js
+++ assignment2.js
@@ -22,6 +22,42 @@ class Cube extends Shape {
     }
 }
 
+//NEW SHAPES
+class Rectangle extends Shape {
+    constructor() {
+        super("position", "normal",);
+        // Loop 3 times (for each axis), and inside loop twice (for opposing cube sides):
+        this.arrays.position = Vector3.cast(
+            [-1, -2, 1], [1, -2, 1], [-1, -2, 1], [1, -2, 1], [1, 2, 1], [-1, 2, -1], [1, 2, 1], [-1, 2, 1],
+            [-1, -2, -1], [-1, -2, 1], [-1, 2, -1], [-1, 2, 1], [1, -2, 1], [1, -2, -1], [1, 2, 1], [1, 2, -1],
+            [-1, -2, 1], [1, -2, 1], [-1, 2, 1], [1, 2, 1], [1, -2, -1], [-1, -2, -1], [1, 2, -1], [-1, 2, -1]);
+        this.arrays.normal = Vector3.cast(
+            [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0],
+            [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0],
+            [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, -1], [0, 0, -1], [0, 0, -1], [0, 0, -1]);
+        // Arrange the vertices into a square shape in texture space too:
+        this.indices.push(0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
+            14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22);
+    }
+}
+
+class Walk extends Shape {
+    constructor() {
+        super("position", "normal",);
+        // Loop 3 times (for each axis), and inside loop twice (for opposing cube sides):
+        this.arrays.position = Vector3.cast(
+            [-3, -1, -.1], [3, -1, -.1], [-3, -1, .1], [3, -1, .1], [.5, 5, -.1], [-.5, 5, -.1], [.5, 5, .1], [-.5, 5, .1],
+            [-3, -1, -.1], [-3, -1, .1], [-.5, 5, -.1], [-.5, 5, .1], [3, -1, .1], [3, -1, -.1], [.5, 5, .1], [.5, 5, -.1],
+            [-3, -1, .1], [3, -1, .1], [-.5, 5, .1], [.5, 5, .1], [3, -1, -.1], [-3, -1, -.1], [.5, 5, -.1], [-.5, 5, -.1]);
+        this.arrays.normal = Vector3.cast(
+            [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0],
+            [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0],
+            [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, -1], [0, 0, -1], [0, 0, -1], [0, 0, -1]);
+        // Arrange the vertices into a square shape in texture space too:
+        this.indices.push(0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
+            14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22);
+    }
+}
 class Cube_Outline extends Shape {
     constructor() {
         super("position", "color");
@@ -53,6 +89,9 @@ class Base_Scene extends Scene {
         this.shapes = {
             'cube': new Cube(),
             'outline': new Cube_Outline(),
+            'rectangle': new Rectangle(),
+            'walkway': new Walk(),
+
         };
 
         // *** Materials
@@ -116,13 +155,47 @@ export class Assignment2 extends Base_Scene {
         return model_transform;
     }
 
+    draw_walkway(context, program_state, walkway_transform)  {
+        const gray = hex_color("#bebebe");
+        let w_Sc = Mat4.scale(8,3,0,1);
+        let w_Tr = Mat4.translation(-4,0,0,1);
+        walkway_transform = walkway_transform.times(w_Tr).times(w_Sc);
+        this.shapes.walkway.draw(context, program_state, walkway_transform, this.materials.plastic.override({color: gray}));
+
+        //SKY
+        let sky_transform = Mat4.identity();
+        const blue = hex_color("#1a9ffa");
+        let sky_Tr = Mat4.translation(-4,20,0,1);
+        let sky_Sc = Mat4.scale(5,15,0,1);
+        let sky_Rt = Mat4.rotation(Math.PI/2, 0, 0, 1);
+        sky_transform = sky_transform.times(sky_Tr).times(sky_Rt).times(sky_Sc);
+        this.shapes.rectangle.draw(context, program_state, sky_transform, this.materials.plastic.override({color: blue}));
+
+        // //BUSHES
+        let bush1_transform = Mat4.identity();
+        let bush1_Tr = Mat4.translation(-17,-8.5,1,1);
+        let bush1_Rt = Mat4.rotation(-Math.PI/3.9,0,0,1);
+        let bush1_Sc = Mat4.scale(.8,6.5,1,1)
+        bush1_transform = bush1_transform.times(bush1_Rt).times(bush1_Tr).times(bush1_Sc);
+        this.shapes.rectangle.draw(context, program_state, bush1_transform, this.materials.plastic.override( {color: hex_color("#0a4915")}));
+
+        let bush2_Tr = Mat4.translation(11, -2, 1, 1);
+        let bush2_Rt = Mat4.rotation(Math.PI/3.9,0,0,1);
+        let bush2_Sc = bush1_Sc;
+        let bush2_transform = Mat4.identity();
+        bush2_transform = bush2_transform.times(bush2_Rt).times(bush2_Tr).times(bush2_Sc);
+        this.shapes.rectangle.draw(context, program_state, bush2_transform, this.materials.plastic.override( {color: hex_color("#0a4915")}));
+
+    }
+
+
     display(context, program_state) {
         super.display(context, program_state);
-        const blue = hex_color("#1a9ffa");
         let model_transform = Mat4.identity();
 
-        // Example for drawing a cube, you can remove this line if needed
-        this.shapes.cube.draw(context, program_state, model_transform, this.materials.plastic.override({color:blue}));
-        // TODO:  Draw your entire scene here.  Use this.draw_box( graphics_state, model_transform ) to call your helper.
+        let walkway_transform = Mat4.identity();
+        let sky_transform = Mat4.identity();
+
+        this.draw_walkway(context, program_state, walkway_transform);
     }
 }
