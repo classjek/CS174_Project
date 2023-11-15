08bfe6528a5f0fbc12a6d0492d78c643000eb435
pinklz <116235230+pinklz@users.noreply.github.com>
2023-11-15 13:36:27 -0800

    Fix file name change issue

--- bruinRun.js
+++ bruinRun.js
@@ -182,7 +182,7 @@ class Base_Scene extends Scene {
     }
 }
 
-export class Assignment2 extends Base_Scene {
+export class BruinRun extends Base_Scene {
     /**
      * This Scene object can be added to any display canvas.
      * We isolate that code so it can be experimented with on its own.
